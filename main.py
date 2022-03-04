b4a2d578f64d195d31c4dc3d24065eef43d53031
Max Cpp <maxvoloshchuk1@gmail.com>
2022-03-04 15:02:37 +0200

    Co-authored-by: MadMaus <MadMaus@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,35 +1,86 @@
 from telethon import TelegramClient
-from telethon.tl.functions.channels import JoinChannelRequest
-from telethon.tl.functions.channels import LeaveChannelRequest
+from telethon.tl.functions.channels import JoinChannelRequest, LeaveChannelRequest
+from telethon.errors.rpcerrorlist import FloodWaitError, PeerFloodError
+from telethon.errors.common import MultiError
 import time
+import threading
 
 api_id = 16321102
 api_hash = 'd94026e2a2f7b4e4be0fde40069782de'
-client = TelegramClient('anon', api_id, api_hash)
+client = TelegramClient('anti_putin', api_id, api_hash)
+
+#Is iptional
+#client.flood_sleep_threshold = 0
 
 async def main():
-    # Getting information about yourself
+    print("ALL OK")
+
     me = await client.get_me()
+    me = await client.get_entity(me)
 
-    username = me.username
-    print(username)
-    print(me.phone)
+    global chanel_of_propaganda
+    global chanel_of_chats
+    global victim
+    global propaganda
 
-    chanel_of_chats = await client.get_entity('https://t.me/chat_of_channels')
+    try:
+        victim = await client.get_entity('https://t.me/chat_of_channels')
+        chanel_of_chats = await client.get_entity('https://t.me/chat_of_channels')
+        chanel_of_propaganda = await client.get_entity('https://t.me/pro_pagandaua')
+    except FloodWaitError as e:
+            print('Have to sleep', e.seconds, 'seconds')
+            time.sleep(e.seconds+0.5)
+    except PeerFloodError:
+            print("SPAM ERROR, U get banned, check out @SpamBot for details")
+            raise SystemExit
+    except MultiError:
+            print("MultiError, U get banned, check out @SpamBot for details")
+            raise SystemExit
 
-    victim = await client.get_entity('+380 67 830 53 24')
-    #channel = await client.get_entity('https://t.me/pro_pagandaua')
     #await client(JoinChannelRequest(victim))
-
-    i = 2
-    while True:
-        await client.forward_messages(victim, i, chanel_of_chats)    
-        await client.send_message(victim, str(i))
-        #time.sleep(0)
-        i += 1
-
-
-    #await client(LeaveChannelRequest(entity))
     
+    #trying to cahs everything
+    
+    users = client.iter_participants(victim, aggressive = True)
+    propaganda = client.iter_messages(chanel_of_propaganda)
+
+    #await client(LeaveChannelRequest(victim))
+
+    try:
+        async for user in users:
+                    if user.id == me.id:
+                        continue
+                    await attack(user)
+            # maybe block the user after attack
+    except FloodWaitError as e:
+            print('Have to sleep', e.seconds, 'seconds')
+            time.sleep(e.seconds+0.5)
+    except PeerFloodError:
+            print("SPAM ERROR, U get banned, check out @SpamBot for details")
+            raise SystemExit
+    except MultiError:
+            print("MultiError, U get banned, check out @SpamBot for details")
+            raise SystemExit
+    
+    
+
+
+async def attack(user):
+
+    async for message in propaganda:
+        try:
+            await client.forward_messages(user, message)    
+            time.sleep(0.1)
+        except FloodWaitError as e:
+            print('Have to sleep', e.seconds, 'seconds')
+            time.sleep(e.seconds+0.5)
+        except PeerFloodError:
+            print("SPAM ERROR, U get banned, check out @SpamBot for details")
+            raise SystemExit
+        except MultiError:
+            print("MultiError, U get banned, check out @SpamBot for details")
+            raise SystemExit
+
+
 with client:
     client.loop.run_until_complete(main())
